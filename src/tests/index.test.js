07eefd95284892bb10ebf2203c608fa2c89d8784
aurimas-darguzis <aurimasdarguzis@gmail.com>
2021-01-11 13:39:48 +0000

    fixed typo

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -1,8 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import Home from "../../pages/index";
 
-// Due to time restrain I bumped into a probel and didn't manage to setup react
-// testing library for this Next.js app
+// Due to time restrain I didn't manage to setup react testing library for this Next.js app
 
 describe("Home", () => {
   it("renders without crashing", () => {
